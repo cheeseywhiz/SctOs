69b59909ac39a7429fc3a9790a527a95411bd0d0
Spencer Todd <cheesy.mac.dott@gmail.com>
2020-04-25 17:37:28 -0400

    add some xv6 libc functions

---
+++ include/x86.h
@@ -0,0 +1,21 @@
+/* copied from xv6 */
+/* Routines to let C code use special x86 instructions. */
+#include <stddef.h>
+
+static inline void
+stosb(void *addr, int data, size_t cnt)
+{
+  asm volatile("cld; rep stosb" :
+               "=D" (addr), "=c" (cnt) :
+               "0" (addr), "1" (cnt), "a" (data) :
+               "memory", "cc");
+}
+
+static inline void
+stosl(void *addr, int data, size_t cnt)
+{
+  asm volatile("cld; rep stosl" :
+               "=D" (addr), "=c" (cnt) :
+               "0" (addr), "1" (cnt), "a" (data) :
+               "memory", "cc");
+}
